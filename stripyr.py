15eb50dc29280e737dbb927d0b5724a1b6a7ca6f
Kevin Kowalczyk <kowalczykrkevin@gmail.com>
2016-01-05 20:51:49 -0500

    Initial Commit
    Initial Commit containing basic script, GNU GPL v3, etc.

---
+++ stripyr.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+"""
+	stripyr is a program that strips extra data from a text file
+	Copyright 2016 Kevin R. Kowalczyk
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+from sys import argv
+
+script, target, dest = argv
+
+in_file = open(target)
+out_file = open(dest, 'w')
+
+print "Ready, hit RETURN to continue, CTRL-C to abort."
+raw_input()
+
+for line in in_file:
+	text = line.split(' ')
+	text[0] += '\n'
+	out_file.write(text[0])
+
+in_file.close()
+out_file.close()
